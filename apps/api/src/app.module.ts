df083b74e0575e7cce00b059ed3b78e9e4051340
Manuel Seitz <manuuseitz@gmail.com>
2026-05-09 19:55:04 +0000

    refactor(api): update TypeOrm configuration and change database name in docker-compose

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,22 +6,12 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { CartsModule } from "./carts/carts.module";
-import { Cart } from "./carts/entities/cart.entity";
-import { CartItem } from "./carts/entities/item.entity";
 import { ChatsModule } from "./chats/chats.module";
-import { Chat } from "./chats/entities/chat.entity";
-import { Message } from "./chats/entities/message.entity";
 import { EmbeddingsModule } from "./embeddings/embeddings.module";
-import { Fridge } from "./fridges/entities/fridge.entity";
-import { FridgeItem } from "./fridges/entities/item.entity";
 import { FridgesModule } from "./fridges/fridges.module";
-import { OrderItem } from "./orders/entities/item.entity";
-import { Order } from "./orders/entities/order.entity";
 import { OrdersModule } from "./orders/orders.module";
 import { ProductsModule } from "./products/products.module";
-import { Recipe } from "./recipes/entities/recipe.entity";
 import { RecipesModule } from "./recipes/recipes.module";
-import { User } from "./users/entities/user.entity";
 import { UsersModule } from "./users/users.module";
 import { VtexModule } from "./vtex/vtex.module";
 
@@ -29,27 +19,15 @@ import { VtexModule } from "./vtex/vtex.module";
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || "development"}.local`,
     }),
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT) || 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [
-        User,
-        Recipe,
-        Chat,
-        Message,
-        Cart,
-        CartItem,
-        Order,
-        OrderItem,
-        Fridge,
-        FridgeItem,
-      ],
-      synchronize: process.env.NODE_ENV === "production" ? false : true,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        autoLoadEntities: true,
+        synchronize: process.env.NODE_ENV !== "production",
+      }),
     }),
     ScheduleModule.forRoot(),
     RecipesModule,
